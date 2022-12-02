8c79278fa1269c397cb0c98759f323bb3475ff43
Kishan <kishan.rajagunathas@gmail.com>
2022-12-02 14:34:37 -0500

    commit

---
+++ assignment-5/src/App.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div class="app-container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+*,
+::before,
+::after {
+  padding: 0;
+  margin: 0;
+  color: purple;
+  box-sizing: border-box;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  background-color: #000000aa;
+}
+.app-container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
