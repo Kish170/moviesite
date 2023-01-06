46d6a9aeb9b10408c78c831e57f50c97d4b6769c
Kishan Rajagunathas <kishan.rajagunathas@student.tdsb.on.ca>
2023-01-06 11:41:51 -0500

    Installed and set up firebase to get neccesary files and to be able to use firebase components

---
+++ assignment-5/src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVv3QOzsxj_3OhcmBd0hNH2DdLt2I8-JI",
+  authDomain: "ics4u-7d97d.firebaseapp.com",
+  projectId: "ics4u-7d97d",
+  storageBucket: "ics4u-7d97d.appspot.com",
+  messagingSenderId: "690900457077",
+  appId: "1:690900457077:web:8f39d29507e308613eca3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
